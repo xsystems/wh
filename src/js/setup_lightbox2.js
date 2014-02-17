f504c9bf54817a767f93031f42fed4faabbe7543
Koen Boes <koenboes@gmail.com>
2014-02-17 01:37:36 +0100

    Updated lightbox.

--- src/js/setup_lightbox2.js
+++ src/js/setup_lightbox2.js
@@ -1,8 +1,6 @@
-var lightboxCSS = "/lib/js/lightbox2/css/lightbox.css";
-var jqueryJS = "/lib/js/lightbox2/js/jquery-1.7.2.min.js";
-var jqueryUIJS = "/lib/js/lightbox2/js/jquery-ui-1.8.18.custom.min.js";
-var jqueryScrollJS = "/lib/js/lightbox2/js/jquery.smooth-scroll.min.js";
-var lightboxJS = "/lib/js/lightbox2/js/lightbox.js";
+var lightboxCSS =   "/lib/js/lightbox/css/lightbox.css";
+var jqueryJS =      "/lib/js/lightbox/js/jquery-1.10.2.min.js";
+var lightboxJS =    "/lib/js/lightbox/js/lightbox-2.6.min.js";
 
 var linkRel = 'stylesheet';
 var linkType = 'text/css';
@@ -10,8 +8,6 @@ var linkMedia = 'screen';
 
 var loader = new Loader();
 window.onload = loader.loadScript(lightboxJS,
-                    loader.loadScript(jqueryScrollJS,
-                        loader.loadScript(jqueryUIJS,
-                            loader.loadScript(jqueryJS,
-                                loader.loadLink(lightboxCSS, linkRel, linkType, linkMedia, "")))));
+                    loader.loadScript(jqueryJS,
+                        loader.loadLink(lightboxCSS, linkRel, linkType, linkMedia, "")));
 
